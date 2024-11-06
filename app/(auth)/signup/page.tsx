06d80cd4b1a334e8036f88701737532bee65044a
AmitBiswas1402 <amit.142biswas@gamil.com>
2024-11-06 20:56:54 +0530

    login and signup made

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -11,7 +11,7 @@ function page() {
     <div className="mt-24 rounded bg-black/80 py-10 px-6 md:mt-0 md:max-w-sm md:px-14">
       <form action="">
         <h1 className="text-3xl font-semibold text-white mb-4 text-center">
-          Log In
+          Sign Up
         </h1>
         <div>
           <Input
@@ -25,7 +25,7 @@ function page() {
             variant="destructive"
             className="w-full bg-[#e50914] mb-2"
           >
-            Sign Up
+            Log In
           </Button>
         </div>
       </form>
